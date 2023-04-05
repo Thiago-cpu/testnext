bfb9a958c5091fe0ac0d0cecd951364298e376d7
thiago <thiago@dreamcode.io>
2023-04-05 16:36:36 -0300

    add test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [name, setName] = useState("loading...");
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.name) {
+          setName(res.name);
+        }
+      });
+  }, []);
   return (
     <>
       <Head>
@@ -19,6 +30,7 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
+            My name is {name}
           </p>
           <div>
             <a
@@ -26,7 +38,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +131,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
